55e85984883d8dedc2de1015bb49fa5dc64a8494
Kyle <kyleairwin@gmail.com>
2015-11-10 22:31:56 +0000

    updates

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 from flask import Flask, render_template
 import os
 from bokeh.charts import Bar, output_file, show
-import json
+import pandas as pd
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     soql_params = {"$select" : "policebeat AS district,count(*)",
                    "$group" : "policebeat"}
     r = requests.get("https://data.oaklandnet.com/resource/ym6k-rx7a.json",
                      params=soql_params)
-    labels = []
-    vals = []
-    for d in r.json():
-        labels.append(d.get('district', False))
-        vals.append(d.get('count', 0))
-    
-    p = Bar(r.json(), 'district', values='count', title='Oakland Crime Totals by Police District')
+    df = pd.read_json(r.json())
+    p = Bar(df, 'district', values='count',
+            title='Oakland Crime Totals by Police District')
     output_file('templates/index.html')
     show(p)
+    
     return render_template('index.html')
     
 if __name__ == '__main__':
