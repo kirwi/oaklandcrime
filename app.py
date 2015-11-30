4877d24c31173544d13ed20e6e3dcd3b5baffb82
blahreport <blahreport@gmail.com>
2015-11-30 12:39:11 -0800

    updated plot

--- app.py
+++ app.py
@@ -1,25 +1,74 @@
-from flask import Flask, render_template
-import os
-from bokeh.charts import Bar, output_file, show
 import pandas as pd
+from bokeh.plotting import figure, output_file, save
 import requests
+from flask import Flask, render_template
+from datetime import datetime
+from math import pi
+from bokeh.models import HoverTool, ColumnDataSource
+import os
+import time
 
 app = Flask(__name__)
 
+
+# Read in the data from a SODA API
+url = "https://data.oaklandnet.com/resource/ym6k-rx7a.json"
+query = {"$select" : "date_trunc_ymd(datetime) AS date,count(*)",
+        "$group" : "date",
+        "$where" : "date < \'%s\'" % time.strftime("%Y-%m-%d"),
+        "crimetype" : "ROBBERY"}
+r = requests.get(url, params=query)
+
+# Create a pandas dataframe, then a bokeh ColumnDataSource for use with 
+# the HoverTool. Convert columns to appropriate data types and sort by date
+df = pd.DataFrame(r.json())
+df['count'] = df['count'].astype(int)
+df['date'] = map(lambda x: datetime.strptime(str(x), 
+    '%Y-%m-%dT%H:%M:%S'), df['date'])
+df = df.sort('date')
+
+# source = ColumnDataSource(
+#         data=dict(
+#             date=df['date'],
+#             date_str=map(lambda x: datetime.strftime(x,'%m/%d'),
+#                 df['date']),
+#             count=df['count']))
+
+hover = HoverTool(tooltips=[
+            ("date", "@date_str"),
+            ("total", "@count")])
+
+# Make it look nice. 'x_axis_type=datetime' allows for x-axis arrays
+# of datetime.datetime objects
+fig = figure(width=1000, x_axis_type='datetime', tools=[hover],
+        title='Robberies in Oakland',
+        title_text_align='center',
+        title_text_font='monospace',
+        title_text_color='Tomato',
+        title_text_font_size='28pt',
+        title_text_font_style='bold',
+        y_axis_label='no. committed',
+        #x_axis_label='date')
+fig.xaxis.axis_label_text_font='monospace'
+fig.xaxis.axis_label_text_font_style='bold'
+fig.yaxis.axis_label_text_font_style='bold'
+fig.yaxis.axis_label_text_font='monospace'
+fig.xaxis.major_label_orientation = pi/4
+fig.background_fill = 'WhiteSmoke'
+fig.grid.grid_line_color = 'White'
+fig.grid.grid_line_width = 2
+fig.grid.grid_line_dash = [6,4]
+
+# Create the plot
+fig.line('date', 'count', line_width=4, line_join='round',
+        line_cap='round', line_color='DeepSkyBlue', source=source)
+fig.circle('date', 'count', size=10, fill_color='White',
+        line_color='Tomato', source=source)
+save(fig, 'templates/index.html')
+
 @app.route('/')
-@app.route('/index')
-def index():
-    soql_params = {"$select" : "policebeat AS district,count(*)",
-                   "$group" : "policebeat"}
-    r = requests.get("https://data.oaklandnet.com/resource/ym6k-rx7a.json",
-                     params=soql_params)
-    df = pd.read_json(r.json())
-    p = Bar(df, 'district', values='count',
-            title='Oakland Crime Totals by Police District')
-    output_file('templates/index.html')
-    show(p)
-    
+def index():    
     return render_template('index.html')
-    
+
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP','0.0.0.0'),port=int(os.getenv('PORT','8080')))
+    app.run()
